2ba9041245161e5cf1e059ed674ab0f525f436cf
zhou-beep <13263107657@163.com>
2020-01-13 17:59:56 +0800

    reporid

---
+++ src/test/java/com/fh/Test.java
@@ -0,0 +1,14 @@
+package com.fh;
+
+/**
+ * @ClassName Test
+ * @Description: TODO
+ * @Author 马传洲
+ * @Date 2020/1/13
+ * @Version V1.0
+ **/
+public class Test {
+    public void Test(){
+        System.out.println("123");
+    }
+}
